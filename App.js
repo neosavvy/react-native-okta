753d514cdc6f7f9ad16dfef772330e4237920325
Adam Parrish <adam@indeelabs.io>
2019-03-18 16:18:45 -0400

    Integrating Pod References for All Libraries to date.

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Caring</Text>
+        <Text style={styles.welcome}>Caring Works</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
