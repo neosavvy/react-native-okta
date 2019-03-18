759f38b756b3ff9f9247987dce7712ad6eb7e2ba
Adam Parrish <adam@indeelabs.io>
2019-03-18 18:22:48 -0400

    All working in iOS except Okta

--- ios/CCC/AppDelegate.h
+++ ios/CCC/AppDelegate.h
@@ -7,11 +7,8 @@
 
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-#import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
-
-@property (nonatomic, weak) id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
   
 @property (nonatomic, strong) UIWindow *window;
 
